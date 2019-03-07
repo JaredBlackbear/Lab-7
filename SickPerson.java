35271fa6fe7cd611f89d5bce83d6b12e2f30ad0e
Jared Blackbear <jared.blackbear@ou.edu>
2019-03-07 12:41:25 -0600

    inital commit

---
+++ SickPerson.java
@@ -0,0 +1,15 @@
+
+public class SickPerson extends Person {
+	private String name="";
+	private int age=0;
+	private int severity=0;
+	
+public SickPerson(String name,int age,int severity) {
+//	this.name=name;
+	super(name,age);
+	this.severity=severity;
+//	this.age=age;
+//	this.severity=severity;
+}
+protected compareto()
+}
